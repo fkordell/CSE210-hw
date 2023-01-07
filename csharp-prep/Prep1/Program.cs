869bba4e417f3959683995e09b838424696d64a2
fkordell <far18009@byui.edu>
2023-01-07 10:36:13 -0700

    Parts 1 & 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string name = Console.ReadLine();
+        Console.Write("What is your last name? ");
+        string name1 = Console.ReadLine();
+        Console.WriteLine($"Yuor name is {name1}, {name} {name1}.");
     }
 }
